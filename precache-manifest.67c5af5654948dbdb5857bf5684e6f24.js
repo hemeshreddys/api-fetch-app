443df8470f9d1d849e12bfff06c6109c271113c7
Hemesh <hemesh.reddy@cheproximity.com.au>
2020-10-10 00:14:52 +1100

    Updates

---
+++ precache-manifest.67c5af5654948dbdb5857bf5684e6f24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a31bd9b3894872cdb0f3",
+    "url": "/api-fetch-app/static/js/runtime~main.adea5ce4.js"
+  },
+  {
+    "revision": "80b64ceb315f5fe8b29c",
+    "url": "/api-fetch-app/static/js/main.f7af74ac.chunk.js"
+  },
+  {
+    "revision": "ee1b43035ec2a8bb5712",
+    "url": "/api-fetch-app/static/js/2.06aa3286.chunk.js"
+  },
+  {
+    "revision": "80b64ceb315f5fe8b29c",
+    "url": "/api-fetch-app/static/css/main.4aa0ca43.chunk.css"
+  },
+  {
+    "revision": "835ccf27fa912f3f1996ece898a10aae",
+    "url": "/api-fetch-app/index.html"
+  }
+];
